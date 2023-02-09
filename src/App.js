8534b2f3f785488b8354e4e5a4f5da0d4eecde7a
Shruti0026 <naik.shrutika026@gmail.com>
2023-02-09 18:20:02 +0530

    routes edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css'
-import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Userdata from './components/Userdata';
 import Header from './components/Header';
 
+
+
 function App() {
     return ( 
-      <div className = 'App'>
-        <Header />
-          <Router>
-            <Routes>
-                <Route path='/Login' element={<Login/>}></Route>
-                <Route path='/Signup' element={<Signup/>}></Route>
-                <Route path='/Userdata' element={<Userdata/>}></Route>    
-            </Routes>
+      <div className = 'App' >
+          <Header />
+          <Router >
+              <Routes >
+                  <Route path = '/Login'element = {<Login/>}> </Route> 
+                  <Route path = '/Signup'element = {<Signup/>}> </Route>
+                  <Route path = '/Userdata' element = {<Userdata/>} > </Route>          
+              </Routes>
+
           </Router>
-  
-        
+
+
       </div>
     );
 }
